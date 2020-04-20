f2a23f119bbe5b0c22e13d76a6c8aae76b339dd0
Tailtati <38712215+Tailtati@users.noreply.github.com>
2020-04-20 13:08:59 -0500

    primera practica javascritp

---
+++ js/mi-script.js
@@ -0,0 +1,15 @@
+let numero1 = prompt ("digite numero 1") //variables//
+let numero2 = prompt ("digite numero 2")
+const suma = numero1 +numero2
+
+numero1 = 10
+
+//console.log("La sumas es: " + suma)//
+
+//console.log(4 > 9) true, false//
+//estructura de un condicional//
+if(numero1 >= numero2){
+    console.log(numero1," es mayor o igual a ", numero2);
+}else{
+    console.log(numero1,  " no es mayor ni igual a ",  numero2);
+}
